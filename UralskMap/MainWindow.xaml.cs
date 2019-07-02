52445e4c7a3573dd6577c4ffef20e896f5bce4dd
Nurik <Nurik@LAPTOP-4QV9CRGE>
2019-07-02 16:49:10 +0300

    video

---
+++ UralskMap/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Microsoft.Maps.MapControl.WPF;
+
+namespace UralskMap
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            myMap.Mode = new RoadMode();
+            bgVideo.Play();
+        }
+    }
+}
